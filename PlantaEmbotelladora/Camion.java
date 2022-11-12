261a22258f782cff1f9d873b96083de688b7c5ef
constanza.giacoboni <constanza.giacoboni@whitebox.com.ar>
2022-11-12 18:28:49 -0300

    run de camion

--- PlantaEmbotelladora/Camion.java
+++ PlantaEmbotelladora/Camion.java
@@ -1,7 +1,20 @@
 package PlantaEmbotelladora;
 
 public class Camion implements Runnable{
-    public void run(){
+    private Almacen unAlmacen;
 
+    public Camion(Almacen almacen){
+        this.unAlmacen = almacen;
+    }
+
+    public void run(){
+        while(true){
+            try {
+                unAlmacen.sacarCaja();
+                System.out.println("--EL CAMION SE LLEVO LAS CAJAS--");
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
     }
 }
