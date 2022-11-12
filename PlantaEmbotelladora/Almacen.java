1790eaad36b4e81bc04e188bbba5a5dfbcd819a2
Franco Marquez <81998373+FrancoNMarquez@users.noreply.github.com>
2022-11-12 18:11:47 -0300

    Seguimos con la parte de almacen y camion
    Avances

--- PlantaEmbotelladora/Almacen.java
+++ PlantaEmbotelladora/Almacen.java
@@ -1,6 +1,62 @@
 package PlantaEmbotelladora;
+import java.security.PublicKey;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 public class Almacen {
+    private Caja[] arregloDeCajas= new Caja[10];
+    private final int MAX_CAJAS = 10;
+    private int cantCajas = 0;
+    Lock lock = new ReentrantLock(true);
+    Condition camionEspera = lock.newCondition();
+    Condition empEspera = lock.newCondition();
+    boolean espacioLibre = true;
+    boolean retirandoCajas = false;
 
+
+    public Almacen(){
+
+    }
+    public boolean estaLleno(){
+        return cantCajas==MAX_CAJAS;
+    }
+    public void ponerCaja(Caja laCaja) throws InterruptedException {
+        lock.lock();
+
+        while(retirandoCajas){
+            empEspera.await();
+        }
+
+        if(this.estaLleno()){
+            camionEspera.signal();
+        }
+        while(this.estaLleno()){
+            empEspera.await();
+        }
+        this.agregarCaja(laCaja);
+
+        lock.unlock();
+    }
+    public void agregarCaja(Caja laCaja){
+        //agregar la caja adentro del array de cajas
+
+    }
+    public void sacarCaja() throws InterruptedException {
+        lock.lock();
+
+        while(!this.estaLleno()){
+            camionEspera.await();
+        }
+        this.vaciarCajas();
+        empEspera.signal();
+        lock.unlock();
+
+
+        lock.unlock();
+    }
+    public void vaciarCajas(){
+        for (int i = 0; i <=MAX_CAJAS ; i++) {
+            arregloDeCajas[i]=null;
+        }
+    }
 }
